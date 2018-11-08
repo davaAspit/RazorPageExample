6770dd34955f3fb66b9fe8158689c0274547f535
Daniel Valsby-Koch <dava@aspit.dk>
2018-11-08 14:05:12 +0100

    Add project files.

---
+++ RazorPageExample/Model/Cat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPageExample.Model
+{
+    public class Cat
+    {
+        public Cat(string name, string imageUrl, string description)
+        {
+            Name = name;
+            ImageUrl = imageUrl;
+            Description = description;
+        }
+
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ RazorPageExample/Model/CatRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPageExample.Model
+{
+    public class CatRepository
+    {
+        public List<Cat> Cats { get; set; } = new List<Cat>();
+
+        public CatRepository()
+        {
+            GenerateCats();
+        }
+
+        private void GenerateCats()
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                Cats.Add(new Cat($"Cat{i}", $"/Images/cat{i}.jpg", "Katten kat er en kat"));
+            }
+
+        }
+    }
+}
